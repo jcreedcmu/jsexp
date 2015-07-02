d3c37264d5fe6001a11730f458a86d0ffbad4bcd
Jason Reed <jcreed@gmail.com>
2015-07-02 11:11:09 -0400

    fn

--- parse.js
+++ parse.js
@@ -4,10 +4,9 @@ var parser = peg.buildParser(fs.readFileSync("grammar.peg", "utf8"))
 var result = parser.parse(fs.readFileSync("input.txt", "utf8"))
 render(result)
 function render(sexps) {
-sexps.forEach(render_one)
-}
-function render_one(sexp) {
+sexps.forEach(function (sexp) {
 console.log(compile(sexp))
+})
 }
 function stringify_str_elm(e) {
 if (typeof(e) == "string") {return(JSON.stringify(e))} else {return(compile(e))}
@@ -22,5 +21,6 @@ if (sexp[0] == "def") {return("var " + sexp[1] + " = " + compile(sexp[2]))}
 if (sexp[0] == "==") {return(compile(sexp[1]) + " == " + compile(sexp[2]))}
 if (sexp[0] == "str") {return(sexp.slice(1).map(stringify_str_elm).join(" + "))}
 if (sexp[0] == "defn") {return("function " + sexp[1] + "(" + sexp[2].slice(1).join(", ") + ") {\n" + sexp.slice(3).map(compile).join("\n") + "\n}")}
+if (sexp[0] == "fn") {return("function (" + sexp[1].slice(1).join(", ") + ") {\n" + sexp.slice(2).map(compile).join("\n") + "\n}")}
 return(sexp[0] + "(" + sexp.slice(1).map(compile).join(", ") + ")")
 }
