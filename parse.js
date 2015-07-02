e3fcf77c2f118b82fb02785a8bc0fb2255b3c569
Jason Reed <jcreed@gmail.com>
2015-07-02 12:23:00 -0400

    when is now macro

--- parse.js
+++ parse.js
@@ -12,6 +12,7 @@ console.log(compile(env, sexp))
 function stringify_str_elm(env,e) {
 if (typeof(e) == "string") {return(JSON.stringify(e))} else {return(compile(env, e))}
 }
+
 function compile(env,sexp) {
 var compile_env = function (sexp) {
 return(compile(env, sexp))
@@ -20,14 +21,13 @@ if (typeof(sexp) == "string") {return(sexp)}
 if (typeof(sexp) == "number") {return(sexp)}
 if (sexp[0]) {if (sexp[0][0] == ".") {return(compile_env(sexp[1]) + sexp[0] + "(" + sexp.slice(2).map(compile_env).join(", ") + ")")}}
 var macro = env[sexp[0]]
-if (macro) {return(compile_env(macro(sexp)))}
+if (macro) {return(compile_env(macro(sexp, compile_env)))}
 if (sexp[0] == "get") {return(compile_env(sexp[1]) + "[" + compile_env(sexp[2]) + "]")}
 if (sexp[0] == "set") {return(compile_env(sexp[1]) + "[" + compile_env(sexp[2]) + "] = " + compile_env(sexp[3]))}
 if (sexp[0] == "debug") {return(JSON.stringify(env))}
 if (sexp[0] == "defmacro") {env[sexp[1]] = eval("(" + compile_env(sexp[2]) + ")")
 return("")}
 if (sexp[0] == "if") {if (sexp[3] == null) {return("if (" + compile_env(sexp[1]) + ") {" + compile_env(sexp[2]) + "}")} else {return("if (" + compile_env(sexp[1]) + ") {" + compile_env(sexp[2]) + "} else {" + compile_env(sexp[3]) + "}")}}
-if (sexp[0] == "when") {return("if (" + compile_env(sexp[1]) + ") {" + sexp.slice(2).map(compile_env).join("\n") + "}")}
 if (sexp[0] == "def") {return("var " + sexp[1] + " = " + compile_env(sexp[2]))}
 if (sexp[0] == "==") {return(compile_env(sexp[1]) + " == " + compile_env(sexp[2]))}
 if (sexp[0] == "str") {return(sexp.slice(1).map(function (exp) {
